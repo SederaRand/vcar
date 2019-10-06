94eb9fb23a28940b04c905ac36ea0b5f6ce84421
SederaRand <randriamanantenasoasedera@gmail.com>
2019-10-06 21:59:08 +0200

    assccommit

---
+++ assets/pouah-estla/pouah-estla.php
@@ -0,0 +1,29 @@
+<?php
+$name = "Pouah Estla"; // Prénom Nom
+$description = "Pouah Estla Conseiller et Manager Immobilier Iad France";
+$categorie = "Conseiller et Manager Immobilier";
+$city = "Paris 75000"; // Ville CodePostal
+$email = "ouipouahestla@gmail.com";
+
+$tel = "(+33) 06 67 17 04 44"; //(+33) 06 25 57 37 78
+$whatsapplink = "https://wa.me/33667170444";//wa.me/numero de telephone sansle plus 3333625573778
+
+$biensimmo = "https://www.iadfrance.fr/conseiller/"; 
+
+$fblink = "https://www.facebook.com/Pouah-Estla-Conseiller-Immobilier-263756767620900/"; //id page à ajouter
+$messengerlink = "https://m.me/Pouah-Estla-Conseiller-Immobilier-263756767620900/"; //id page
+
+$twitterlink = ""; // https://twitter.com/charlieestla
+$linkedinlink = "https://www.linkedin.com/in/j%C3%A9r%C3%A9my-perez-b33a972a/"; //https://www.linkedin.com/in/charlieestla/
+$instalink = "https://www.instagram.com/jeremy_perez_iad_france/"; //https://www.instagram.com/charlieestla/
+
+$immodvisorapi = "5WR4-8GD74-VY3A9-H7L5H8-ZN1C";// (M7SUP4-M2Z9-GP4LP-5N61-557CA) a récupérer sur immodvisor et ajouter sous-domaine à ajouter
+
+$template = "iad"; //or other 
+$bg = "yes"; // Bg personnalisé mettre yes ou no
+
+//faire le vcf sur http://mihalcea.fr/src/vcard/vcardgen.php en pensant à retirer la ligne du site
+//créer photo avec prenom-nompict.jpg
+//créer background avec prenom-nombg.jpg
+
+?>
